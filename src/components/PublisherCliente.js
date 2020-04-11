f41b759095514a0e0d68546a71f4ac769fe1dd6f
paty-caballero <patycaballero.1995@gmail.com>
2020-04-10 20:26:35 -0400

    importar la dependencia de bootstrap

--- src/components/PublisherCliente.js
+++ src/components/PublisherCliente.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 //import { OTPublisher } from 'opentok-react';
 //import CheckBox from './CheckBox';
 
 class PublisherCliente extends React.Component {
+  constructor(props) {
+      super(props)
+      this.clickAgregar = this.clickAgregar.bind(this)
+    }
 
+    clickAgregar(){
+      console.log('boton agregar');
+      this.setState({new:true});
+    }
 
   render() {
     return (
       <div className="publisherCliente">
       Cliente
 
+      <Button href="/Publisher" variant='primary' size='sm'>
+        Link
+      </Button>
 
 
-        <button variant="outlined" color="primary" >Llamar</button>
-
+        <Button variant='primary' block={true}>
+          Primary
+        </Button>
 
 
       </div>
